a293f77348b2f7456b7a8c10271365f0de50298a
Umer <sahilsahil38656@gmail.com>
2023-04-07 10:53:50 +0530

    Initial commit

---
+++ Umer.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b ,c;
+    cout<<"Enter the a  : ";
+    cin>>a;
+    cout<<"Enter the b  : ";
+    cin>>b;
+    cout<<"Enter the c  : ";
+    cin>>c;
+    int sum = 0;
+    sum  += a + b +c;
+    cout<<"This is total sum : "<<sum<<endl;
+}
